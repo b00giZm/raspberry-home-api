b2b313db1e010e04b2aa58755ae010f8a76eefdf
Pascal Cremer <b00gizm@gmail.com>
2014-10-26 00:38:02 +0200

    [Sonos] Testing device search

--- app/routes/sonos.js
+++ app/routes/sonos.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var Sonos = require('sonos').Sonos;
+var search = require('sonos').search;
 
 var router = express.Router();
+
+var sonosDevices = [];
+search(function(device, model) {
+  console.log(device, model);
+});
 
 router.get('/', function(req, res, next) {
   return res.json({ hello: 'world' });
