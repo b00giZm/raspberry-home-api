782f36c05a5b6c4fec63588a4f39b889481ad809
Pascal Cremer <b00gizm@gmail.com>
2014-10-26 02:05:17 +0200

    [Sonos] Added sonos module for devices in network

---
+++ app/lib/sonos.js
@@ -0,0 +1,72 @@
+var crypto = require('crypto');
+var sonos  = require('sonos');
+var lodash = require('lodash');
+var Q      = require('q');
+
+var devices = {};
+
+function deviceId(serialNumber) {
+  return crypto
+    .createHash('sha1')
+    .update(serialNumber)
+    .digest('hex')
+    .substring(0, 8)
+  ;
+};
+
+function transform(done) {
+  done = done || lodash.noop;
+
+  return function(dev, model) {
+    var device = new sonos.Sonos(dev.host, dev.port);
+
+    return Q
+      .all([
+        Q.ninvoke(device, 'getZoneInfo'),
+        Q.ninvoke(device, 'getZoneAttrs'),
+      ])
+      .then(function(results) {
+        return done(null, {
+          id           : deviceId(results[0].SerialNumber),
+          model        : model,
+          name         : results[1].CurrentZoneName,
+          serialNumber : results[0].SerialNumber,
+          macAddress   : results[0].MACAddress,
+          ipAddress    : results[0].IPAddress,
+          port         : dev.port,
+          version      : results[0].SoftwareVersion
+        });
+      }, function(err) {
+        return done(err);
+      })
+    ;
+  };
+};
+
+var searchNetwork = module.exports.searchNetwork = function() {
+  sonos.search(transform(function(err, device) {
+    if (err) {
+      throw err;
+    }
+
+    devices[device.id] = device;
+  }));
+};
+
+var all = module.exports.all= function() {
+  return lodash
+    .keys(devices)
+    .map(function(key) {
+      return devices[key];
+    })
+  ;
+};
+
+var byId = module.exports.byId = function(deviceId) {
+  return devices[key] || null;
+};
+
+var nativebyId = module.exports.nativebyId = function(deviceId) {
+  var device = byId(deviceId);
+  return device ? new sonos.Sonos(device.ipAddress, device.port) : null;
+};
--- app/routes/sonos.js
+++ app/routes/sonos.js
@@ -1,16 +1,23 @@
 var express = require('express');
-var Sonos = require('sonos').Sonos;
-var search = require('sonos').search;
+var sonos = require('../lib/sonos');
+
+sonos.searchNetwork();
 
 var router = express.Router();
 
-var sonosDevices = [];
-search(function(device, model) {
-  console.log(device, model);
+router.get('/', function(req, res, next) {
+  return res.json(sonos.all());
 });
 
-router.get('/', function(req, res, next) {
-  return res.json({ hello: 'world' });
+router.get('/:id', function() {
+  var device = sonos.byId(req.params.id);
+  if (!device) {
+    return res.json(404, {
+      error: 'No device found for id ' + req.params.id
+    });
+  }
+
+  return res.json(device);
 });
 
 module.exports = router;
