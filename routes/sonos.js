3ea720b96305ab51663332ec2c89d1196e1d4937
Pascal Cremer <b00gizm@gmail.com>
2014-12-13 12:34:13 +0100

    Added endpoints for currently playing track and album artwork

--- routes/sonos.js
+++ routes/sonos.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var sonos = require('../lib/sonos');
+var Sonos   = require('sonos').Sonos;
+var request = require('request');
+var sonos   = require('../lib/sonos');
 
 sonos.searchNetwork();
 
@@ -10,14 +12,58 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-  var device = sonos.byId(req.params.id);
-  if (!device) {
+  var deviceInfo = sonos.byId(req.params.id);
+  if (!deviceInfo) {
     return res.json(404, {
       error: 'No device found for id ' + req.params.id
     });
   }
 
-  return res.json(device);
+  return res.json(deviceInfo);
+});
+
+router.get('/:id/current_track', function(req, res, next) {
+  var deviceInfo = sonos.byId(req.params.id);
+  if (!deviceInfo) {
+    return res.json(404, {
+      error: 'No device found for id ' + req.params.id
+    });
+  }
+
+  var sonosDevice = new Sonos(deviceInfo.ipAddress, deviceInfo.port);
+  sonosDevice.currentTrack(function(err, trackInfo) {
+    if (err) {
+      console.log(err);
+      return res.json(500, {
+        error: 'Internal error'
+      });
+    }
+
+    return res.json(trackInfo);
+  });
+});
+
+router.get('/:id/current_track/album_art.jpg', function(req, res, next) {
+  var deviceInfo = sonos.byId(req.params.id);
+  if (!deviceInfo) {
+    return res.status(404).end();
+  }
+
+  var sonosDevice = new Sonos(deviceInfo.ipAddress, deviceInfo.port);
+  sonosDevice.currentTrack(function(err, trackInfo) {
+    if (err || !trackInfo || !trackInfo.albumArtURI) {
+      return res.status(404).end();
+    }
+
+    var url = 'http://' + deviceInfo.ipAddress + ':' + deviceInfo.port + trackInfo.albumArtURI;
+
+    return request(url)
+      .on('response', function(response) {
+        console.log(response.statusCode, response.headers);
+      })
+      .pipe(res)
+    ;
+  });
 });
 
 module.exports = router;
